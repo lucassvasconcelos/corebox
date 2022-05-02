5e6af380da37a41f43483becb41fccebdf3fa50f
Lucas Vasconcelos <lucas.vasconcelos@texvn.group>
2022-05-02 19:08:48 -0300

    +semver:feature adiciona validators personalizados para tratar propriedades padrao da entidade

---
+++ src/CoreBox/Domain/Validators/CreateValidator.cs
@@ -0,0 +1,22 @@
+using FluentValidation;
+
+namespace CoreBox.Domain.Validators;
+
+public class CreateValidator<T> : AbstractValidator<T> where T : Entity<T>
+{
+    public CreateValidator(bool doSoftDeleteValidations = true)
+    {
+        RuleFor(r => r.Id).NotEmpty().WithMessage($"Id é obrigatório!");
+        RuleFor(r => r.DataCriacao).InclusiveBetween(DateTime.UtcNow.AddMilliseconds(-2000), DateTime.UtcNow.AddMilliseconds(2000)).WithMessage($"Data de criação é inválida!");
+        RuleFor(r => r.IdUsuarioCriacao).NotEmpty().WithMessage($"Id do usuário de criação é obrigatório!");
+        RuleFor(r => r.DataUltimaAtualizacao).Empty().WithMessage($"Data da última atualização deve ser nula no cadastro!");
+        RuleFor(r => r.IdUsuarioAtualizacao).Empty().WithMessage($"Id do usuário da atualização deve ser nulo no cadastro!");
+        
+        if (doSoftDeleteValidations)
+        {
+            RuleFor(r => r.DataExclusao).Empty().WithMessage($"Data de exclusão deve ser nula no cadastro!");
+            RuleFor(r => r.IdUsuarioExclusao).Empty().WithMessage($"Id do usuário da exclusão deve ser nulo no cadastro!");
+            RuleFor(r => r.FoiExcluido).Equal(false).WithMessage($"Flag se foi excluído sempre precisar ser falsa na criação!");
+        }
+    }
+}
